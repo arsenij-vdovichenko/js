d3b9a5a9aaeb95de8671d265312b2bd27354d43b
arsenij-vdovichenko <vdovicenkoarsenij946@gmail.com>
2025-07-12 12:32:10 +0100

    123

---
+++ js/firstlesson.js
@@ -0,0 +1,69 @@
+//let,const,var
+
+// const firstName = "arsenii";
+// console.log(firstName);
+// let lastName;
+// lastName = "vdovychenko"
+// console.log(lastName);
+
+//сім примітивних типів данних string number bigint boolean symbol null undefined
+
+// const name = "vupsin"
+// console.log(typeof (name));
+// const age = 98
+// console.log(typeof (age));
+
+// console.log(10>5);
+// console.log(10>12);
+// console.log(typeof (10>12));
+// console.log(typeof ($));
+
+// const message =" вИвчаю дЖава скріпТ"
+// console.log(message.length);
+
+// let number = "21"
+// console.log(number);
+
+// const lowerMessage = message.toLowerCase
+// console.log(lowerMessage);
+
+// alert("я вивчАю дЖава скріпт")
+
+// const isAdult = "чи виаовнилось вам 18 років"
+// const result = confirm(isAdult)
+// console.log(result);
+
+// const myAnswer = "введіть ваше ім'я та прізвище"
+// const userAnswer = prompt(myAnswer)
+// console.log(userAnswer);
+
+// let number = 25;
+// number = number + 10;
+// console.log(number);
+
+//Виведіть в консоль через кому рядок “Я навчаюся в GoITeens в групі: ” і змінну group в якої значення це назва твоєї групи FE_1Y_19_22_06_24.
+// const group = "FE_1Y_19_22_06_24"
+// console.log("Я навчаюся в GoITeens в групі:" +group);
+
+//Створіть дві змінні, в яких буде зберігатись ваш вік і вік вашого друга. Сумму двох змінних виведіть на екран за допомогою console.log().Написати скрипт, який виводить в консоль добуток двох змінних a і b.
+ 
+// const myAge = 14;
+// const friendAge = 13;
+// const suma = myAge + friendAge;
+// console.log(suma);
+
+
+//За допомогою prompt запитайте ім’я користувача. Отримане значення виведіть в консоль великими літерами.
+ 
+// const myYears = " ваше ім’я "
+// const userYears = prompt(myYears)
+// const messageUpperCase = userYears.toUpperCase()
+
+// console.log(messageUpperCase);
+
+//Оголоси дві змінні, які зберігають назву та ціну товару: name і price. Присвойте змінним наступні характеристики товару (відразу при оголошенні) назва: MacBook Pro 13″, ціна: 100000
+//Використовуючи стандартний рядок виведи в консоль інформацію про товар, вийде: 'Обрано «MacBook Pro 13″», ціна за штуку 10000 кредитів'. Присвой товару нову ціну - 20000. Використовуючи стандартний рядок виведи в консоль інформацію про товар, вийде: 'Обрано «MacBook Pro 13″», ціна за штуку 2000 кредитів'.
+
+// const nameGoods = "«MacBook Pro 13″»"
+// let priceGoods = 10000;
+// console.log("обранно" + nameGoods + "ціна за штуку" + priceGoods + "кредитів");
