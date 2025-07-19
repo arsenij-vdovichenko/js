a72944a4dd4dd91b100715f5e63d1bf9f6b6660e
arsenij-vdovichenko <vdovicenkoarsenij946@gmail.com>
2025-07-19 11:30:50 +0100

    123

--- js/firstlesson.js
+++ js/firstlesson.js
@@ -1,97 +1,97 @@
-//let,const,var
+let,const,var
 
-// const firstName = "arsenii";
-// console.log(firstName);
-// let lastName;
-// lastName = "vdovychenko"
-// console.log(lastName);
+const firstName = "arsenii";
+console.log(firstName);
+let lastName;
+lastName = "vdovychenko"
+console.log(lastName);
 
 //сім примітивних типів данних string number bigint boolean symbol null undefined
 
-// const name = "vupsin"
-// console.log(typeof (name));
-// const age = 98
-// console.log(typeof (age));
+const name = "vupsin"
+console.log(typeof (name));
+const age = 98
+console.log(typeof (age));
 
-// console.log(10>5);
-// console.log(10>12);
-// console.log(typeof (10>12));
-// console.log(typeof ($));
+console.log(10>5);
+console.log(10>12);
+console.log(typeof (10>12));
+console.log(typeof ($));
 
-// const message =" вИвчаю дЖава скріпТ"
-// console.log(message.length);
+const message =" вИвчаю дЖава скріпТ"
+console.log(message.length);
 
-// let number = "21"
-// console.log(number);
+let number = "21"
+console.log(number);
 
-// const lowerMessage = message.toLowerCase
-// console.log(lowerMessage);
+const lowerMessage = message.toLowerCase
+console.log(lowerMessage);
 
-// alert("я вивчАю дЖава скріпт")
+alert("я вивчАю дЖава скріпт")
 
-// const isAdult = "чи виаовнилось вам 18 років"
-// const result = confirm(isAdult)
-// console.log(result);
+const isAdult = "чи виаовнилось вам 18 років"
+const result = confirm(isAdult)
+console.log(result);
 
-// const myAnswer = "введіть ваше ім'я та прізвище"
-// const userAnswer = prompt(myAnswer)
-// console.log(userAnswer);
+const myAnswer = "введіть ваше ім'я та прізвище"
+const userAnswer = prompt(myAnswer)
+console.log(userAnswer);
 
-// let number = 25;
-// number = number + 10;
-// console.log(number);
+let number = 25;
+number = number + 10;
+console.log(number);
 
 //Виведіть в консоль через кому рядок “Я навчаюся в GoITeens в групі: ” і змінну group в якої значення це назва твоєї групи FE_1Y_19_22_06_24.
-// const group = "FE_1Y_19_22_06_24"
-// console.log("Я навчаюся в GoITeens в групі:" +group);
+const group = "FE_1Y_19_22_06_24"
+console.log("Я навчаюся в GoITeens в групі:" +group);
 
-//Створіть дві змінні, в яких буде зберігатись ваш вік і вік вашого друга. Сумму двох змінних виведіть на екран за допомогою console.log().Написати скрипт, який виводить в консоль добуток двох змінних a і b.
+//творіть дві змінні, в яких буде зберігатись ваш вік і вік вашого друга. Сумму двох змінних виведіть на екран за допомогою console.log().Написати скрипт, який виводить в консоль добуток двох змінних a і b.
  
-// const myAge = 14;
-// const friendAge = 13;
-// const suma = myAge + friendAge;
-// console.log(suma);
+const myAge = 14;
+const friendAge = 13;
+const suma = myAge + friendAge;
+console.log(suma);
 
 
 //За допомогою prompt запитайте ім’я користувача. Отримане значення виведіть в консоль великими літерами.
  
-// const myYears = " ваше ім’я "
-// const userYears = prompt(myYears)
-// const messageUpperCase = userYears.toUpperCase()
+const myYears = " ваше ім’я "
+const userYears = prompt(myYears)
+const messageUpperCase = userYears.toUpperCase()
 
-// console.log(messageUpperCase);
+console.log(messageUpperCase);
 
 //Оголоси дві змінні, які зберігають назву та ціну товару: name і price. Присвойте змінним наступні характеристики товару (відразу при оголошенні) назва: MacBook Pro 13″, ціна: 100000
 //Використовуючи стандартний рядок виведи в консоль інформацію про товар, вийде: 'Обрано «MacBook Pro 13″», ціна за штуку 10000 кредитів'. Присвой товару нову ціну - 20000. Використовуючи стандартний рядок виведи в консоль інформацію про товар, вийде: 'Обрано «MacBook Pro 13″», ціна за штуку 2000 кредитів'.
 
-// const nameGoods = "«MacBook Pro 13″»"
-// let priceGoods = 10000;
-// console.log("обранно" + nameGoods + "ціна за штуку" + priceGoods + "кредитів");
+const nameGoods = "«MacBook Pro 13″»"
+let priceGoods = 10000;
+console.log("обранно" + nameGoods + "ціна за штуку" + priceGoods + "кредитів");
 
 
 
-// console.log(wath.min(2,5,7,8,9));
-// //Знаходження найменшого числа
-// 3
-// console.log(Math.max (5,12,43,200,54,11.));
-// //Знаходження найбільшого числа
-// console.log();
-// const number = 20
-// const power = 8
-// const result = Math.pow (number,power)
-// console.log(result);
+console.log(wath.min(2,5,7,8,9));
+//Знаходження найменшого числа
+3
+console.log(Math.max (5,12,43,200,54,11.));
+//Знаходження найбільшого числа
+console.log();
+const number = 20
+const power = 8
+const result = Math.pow (number,power)
+console.log(result);
 
-// // піднесення до степення
-// console.log(Math.floor (12.7));
-// //Заокруглення в меншу сторону
-// console.Jog(Math.ceil(47.1));
-// //Заокруглення в більшу сторону
-// console.log(Math.round(55.3));
-// console. log(Math.round(54.49))
-// //Заокруглення ях в математике
-// const num = 15
+// піднесення до степення
+console.log(Math.floor (12.7));
+//Заокруглення в меншу сторону
+console.Jog(Math.ceil(47.1));
+//Заокруглення в більшу сторону
+console.log(Math.round(55.3));
+console. log(Math.round(54.49))
+//Заокруглення ях в математике
+const num = 15
 
-// const sqrt = Math.sqrt(number)
+const sqrt = Math.sqrt(number)
 
-// constRound.number 
-// console.log(sqrt);
+constRound.number 
+console.log(sqrt);
---
+++ js/thirdlesson.js
@@ -0,0 +1,59 @@
+// const message = "i love js"
+// const message2 ="arsenii"
+// const result = message2 +""+ message
+// console.log(result[4]);
+
+// const a = 5 
+// const b = 10 
+// const c = "200"
+// const res = a + b + c
+// console.log(res);
+// console.log(typeof res);
+
+
+// const userName = "Arsenii vdovychenko"
+// console.log(userName.length);//length повератає довжину рядка
+// console.log(userName[userName.length-1]); //[userName.length-1] повертає значення останього елементу в рядку
+
+// const upperText = userName.toUpperCase()//медот який призводить до верхнього регістру
+// console.log(upperText);
+
+// const lowerText = userName.toLowerCase()// медот який призводить до нижнього регістру
+// console.log(lowerText);
+
+
+// const message = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus laboriosam cupiditate mollitia necessitatibus quod! Rerum dolor vel molestiae, ad deleniti eaque maiores nisi id architecto maxime sunt inventore, animi accusantium?"
+// console.log(message[message.length-1]);
+// console.log(message.indexOf(""));// повертає індекс  першого іхлдження якщо не знайшов повертає -1
+// console.log(message.includes(""));// повертає true або false
+// const userEmail = "test@gmail.com";
+// console.log(userEmail.startsWith("test"));
+// console.log(userEmail.endsWith("com"));
+
+// const testText = prompt("ввудіть ваше ім'я").trim();
+// console.log(testText.length);
+
+const userNumber = "8458-59590539"
+console.log(userNumber.length);
+const addedNumber = userNumber.padStart(15,"+38");
+console.log(addedNumber);
+const fixNumber = userNumber.padEnd
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
