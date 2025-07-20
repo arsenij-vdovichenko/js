9f1e50a6b768a2331835f610d787cf5bb047cf66
arsenij-vdovichenko <vdovicenkoarsenij946@gmail.com>
2025-07-20 11:38:31 +0100

    123

--- js/thirdlesson.js
+++ js/thirdlesson.js
@@ -33,11 +33,58 @@
 // const testText = prompt("ввудіть ваше ім'я").trim();
 // console.log(testText.length);
 
-const userNumber = "8458-59590539"
-console.log(userNumber.length);
-const addedNumber = userNumber.padStart(15,"+38");
-console.log(addedNumber);
-const fixNumber = userNumber.padEnd
+// const userNumber = "8458-59590539"
+// console.log(userNumber.length);
+// const addedNumber = userNumber.padStart(15,"+38");
+// console.log(addedNumber);
+// const fixNumber = userNumber.padEnd
+
+
+// Створити змінну, яка містить рядок з вашим ім'ям та прізвищем. Використовуючи метод .length, вивести в консоль кількість символів у рядку.
+// const name = "arsenii"
+// const lastName = "vdovychenko"
+// const result = `${name} ${lastName}`
+// console.log(result.length);
+// console.log(`довжина рядка дорівнює ${result.length}`);
+
+
+//Створити змінну, яка містить рядок з вашим ім'ям. Використовуючи метод .toUpperCase(), перетворити рядок у верхній регістр та вивести результат в консоль.
+// const myName = "arsenii"
+// console.log(myName.toUpperCase());
+
+
+//Створити змінну, яка містить рядок з вашим ім'ям. Використовуючи метод .charAt(), вивести перший символ вашого імені в консоль.
+// const userName = "arsenii"
+// console.log(userName.charAt());
+
+
+//Створити змінну, яка містить рядок з вашим ім'ям та прізвищем. Використовуючи метод .indexOf(), знайти і вивести в консоль позицію, на якій знаходиться пробіл між ім'ям та прізвищем.
+// const userName = "arsenii vdovychenko"
+// console.log(`${userName.indexOf(" ") +1}`);
+
+
+//Створити змінну, яка містить рядок з вашим ім'ям. Використовуючи метод .replace(), замінити першу літеру вашого імені на символ "@" та вивести результат в консоль.
+// const myName= "arsenii"
+// const firstLetter = 
+// console.log(myName.replace("M", "@"));
+
+
+//Створити змінну, яка містить рядок зі словом "JavaScript". Використовуючи метод .substring(), вивести підстроку, яка містить перші 4 символи слова "Java".
+// const message = "JavaScript"
+// console.log(message.substring());
+
+// const message = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita tempora asperiores perferendis, officia nihil placeat beatae, culpa maxime dignissimos assumenda tempore ad? Dolorem minima, tempora architecto magnam harum iste tenetur."
+// const slicemessage = message.slice(0 , 40)
+// console.log(slicemessage
+
+
+
+
+
+
+
+
+
 
 
 
