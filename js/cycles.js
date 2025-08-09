4ba0610ed7b3c6b2cb2772b3d5f03fe8c0ef08e3
arsenij-vdovichenko <vdovicenkoarsenij946@gmail.com>
2025-08-09 11:29:44 +0100

    123

---
+++ js/cycles.js
@@ -0,0 +1,59 @@
+// for (let i = 0; i <= 10; i += 2){
+//     console.log(2);
+    
+// }
+
+// const userInfo = "мене звуть арсеній"
+// console.log(userInfo.length);
+// console.log(userInfo[0]);
+// console.log(userInfo[1]);
+// console.log(userInfo[2]);
+// console.log(userInfo[3]);
+// console.log(userInfo[4]);
+// console.log(userInfo[5]);
+// console.log(userInfo[6]);
+// console.log(userInfo[7]);
+// console.log(userInfo[8]);
+// console.log(userInfo[9]);
+
+// for(let i = 0; i < userInfo.length; i +=1){
+    
+//     if(userInfo[i ===  " "]){
+//         continue
+//     }
+//     console.log(userInfo[i]);
+// }
+
+// for(let i = 0; i < userInfo.length; i +=1){
+    
+//     if(userInfo[i ===  " "]){
+//         continue
+//     }
+//     console.log(userInfo[i]);
+// }
+
+// for (let i = 0; i < userInfo.length; i +=1){
+//     // console.log(i);
+//     console.log(userInfo [i]);
+    
+// }
+
+// // 2. Вивести парні числа від 2 до 20
+// for (let i = 0; i <= 20; i +=1){
+//     if(i % 2 === 1){
+//         continue
+//     }
+//     console.log(i);
+    
+// }
+// let number = 0
+// for (let i = 0; i < 100; i +=1){
+//     number +=1
+// }
+// console.log(number);
+
+// 4. Таблиця множення для 5
+for (let i = 0; i <= 10; i += 1){
+    console.log(`5 x ${i} - ${5 * i}`);
+    
+}
