0f2c88df121f6114402effef91ce446785247c7f
arsenij-vdovichenko <vdovicenkoarsenij946@gmail.com>
2025-08-17 11:33:03 +0100

    123

--- js/masive.js
+++ js/masive.js
@@ -26,8 +26,88 @@
 //     }
 // }
 
-const cart = [54, 28, 105, 70, 92, 17, 120, 12, 25, 90, 44, 99, 87, 34,];
-// cart[]
-cart[cart.length]=1000;
-console.log(cart);
+// const cart = [54, 28, 105, 70, 92, 17, 120, 12, 25, 90, 44, 99, 87, 34,];
+// // cart[]
+// cart[cart.length]=1000;
+// console.log(cart);
 
+// const numbers = [12, 47, 5, 88, 63, 29, 94, 31, 56, 72, 6, 18, 40, 67, 81, 25, 90, 14, 38, 53];
+
+// let minNumbers = numbers[0]
+
+// for(let i = 0; i < numbers.length; i+=){
+    
+// if (minNumbers > numbers[i]){
+//     minNumbers = numbers[i]
+// }
+    
+// }
+// console.log(minNumbers);
+
+//Напиши скрипт який рахує суму елементів двух масивів. Без методу concat()
+// const array1 = [5, 10, 15, 20];
+// const array2 = [10, 20, 30];
+// let sumArray = 0
+// // for (let i = 0; i < array1.length; i += 1){
+// //     // console.log(array1[1]);
+// //     sumArray += array1[i]
+    
+// // }
+// for (let i = 0; i < array2.length; i += 1){
+//     // console.log(array1[1]);
+//     sumArray += array2[i]
+    
+// }
+// console.log(sumArray);
+
+
+// привести номера телфону до міжнародного формату (+38)
+// const phones = [
+//   "0931234567",
+//   "0671112233",
+//   "0501234890",
+//   "0991234765",
+//   "0631112255",
+//   "0971234111",
+//   "0681234999",
+//   "0931112200",
+//   "0661234321",
+//   "0951234654"
+// ];
+
+// for ( let i = 0; i < phones.length; i += 1){
+
+//     console.log(phones[i].padStart('(+38)'));
+    
+// }
+
+
+//знайди під яким індексом знаходиться анастасія
+// const names = [
+//   "Олександр",
+//   "Марія",
+//   "Іван",
+//   "Анастасія",
+//   "Андрій",
+//   "Олена",
+//   "Дмитро",
+//   "Катерина",
+//   "Сергій",
+//   "Юлія",
+// ];
+// for (let i = 0; i < names.length; i += 1){
+//     if (names[i]==="Анастасія")
+//         console.log("ім'я анастасія знаходиться під індексом".i);
+        
+// }
+
+const numbers = [12, 47, 5, 88, 63, 29, 94, 31, 56, 72, 6, 18, 40, 67, 81, 25, 90, 14, 38, 53];
+for(let i = 0; i < numbers.length; i += 1){
+    // console.log(numbers[i]);
+    if (numbers[i] % 2 === 1) {
+        console.log(numbers[i] +=1000);
+        
+    }
+    
+    
+}
