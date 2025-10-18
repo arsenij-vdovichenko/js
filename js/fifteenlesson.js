707325adb0ff60c9e09d6b0ca9e1476bb094a13d
arsenij-vdovichenko <vdovicenkoarsenij946@gmail.com>
2025-10-18 11:35:13 +0100

    123

--- js/fifteenlesson.js
+++ js/fifteenlesson.js
@@ -124,14 +124,14 @@
 
 // //масив.reduce((acc,item,idx,arr)=>{},0)
 
-// порахувати суму усіх елементів
-const numbers = [2, 5, 6, 8, 80, 40];
-const suma =  numbers.reduce((acc,item)=>{
-    return acc+item
-},0)
-console.log(suma);
+// // порахувати суму усіх елементів
+// const numbers = [2, 5, 6, 8, 80, 40];
+// const suma =  numbers.reduce((acc,item)=>{
+//     return acc+item
+// },0)
+// console.log(suma);
 
-//порахувати загальну зарплату
+// //порахувати загальну зарплату
 
 // const salary = {
 //   mango: 30,
@@ -141,3 +141,106 @@ console.log(suma);
 
 // const totaSalary = Object.values(salary).reduce((acc, value)=> acc + value, 0)
 // console.log(totaSalary);
+
+
+
+// //отримати масив усіх tags
+// // const tweets = [
+// //   { id: "000", likes: 5, tags: ["js", "nodejs"] },
+// //   { id: "001", likes: 2, tags: ["html", "css"] },
+// //   { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
+// //   { id: "003", likes: 8, tags: ["css", "react"] },
+// //   { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
+// // ];
+
+// // const newTag - tweets.reduce((acc,item)-> {
+// //   acc.push(...item.tags)
+// // },[])
+
+
+// //створити об'єкт з ключем tags і кількістю повторювань 
+// // {
+// //   js: 3,
+// //   nodejs: 3,
+// //   html: 2,
+// //   css: 2,
+// //   react: 2
+// // }
+
+// const newTags = (array) => array.flatMap(({ tags }) => tags);
+// console.log(newTags(tweets));
+// const objTags = newTags.reduce((acc,item) =>{
+//   if (acc [ item]){
+//     acc[item] +=1;
+//   } else {
+//     acc[item] - 1;
+//   }
+//   return acc;
+// },{})
+// console.log(objTags);
+
+
+
+
+const array = [1,2,3,4,45,6,7,88,9,]
+// const result =array.sort()
+// console.log(result);
+// console.log(array);
+
+// const newArray = array.slice()
+
+
+// const copyArray = [...array]
+// const result = copyArray.sort((a,b)=> a - b)
+// console.log(result);
+
+
+
+// const words = ['кіт', 'собака', 'миша', 'папуга', 'хомяк']
+// const newResult = [...words].sort((a,b)=> a.localeCompare(b))
+// console.log(newResult);
+
+
+// const filteredArray = array.filter((item)=>{
+//   return item >5
+// })
+
+
+
+// const letters = ["A", "a", "B", "b", "C", "c"]
+// console.log(letters.sort());
+
+
+// const players = [
+//   { id: "player-1", name: "Mango", timePlayed: 310, points: 54, online: false },
+//   { id: "player-2", name: "Poly", timePlayed: 470, points: 92, online: true },
+//   { id: "player-3", name: "Kiwi", timePlayed: 230, points: 48, online: true },
+//   { id: "player-4", name: "Ajax", timePlayed: 150, points: 71, online: false },
+//   { id: "player-5", name: "Chelsy", timePlayed: 150, points: 48, online: true },
+// ];
+
+// // const newPlayer = [...players].sort((a,b) => [a.points = b.points])
+// // console.log(copyPlayers);
+// // const copyPlayers = [...players].sort((a,b) => a.name.localeCompare(b.name))
+// // console.log(copyPlayers);
+
+// const copyPlayers = players.slice().sort((a, b)=>{
+// if(a.timePlayed === b.timePlayed){
+//   return a.point - b.point
+// }
+// return a.timePlayed - b.timePlayed
+// })
+
+
+
+// Відсортувати масив об'єктів за алфавітом в полі name:
+const arr = [
+    { name: 'John', age: 32 },
+    { name: 'Jane', age: 26 },
+    { name: 'Mike', age: 42 },
+    { name: 'Emily', age: 29 }
+  ];
+
+  const copyArr = [...arr].sort((a,b)=> a.name.localeCompare(b.name))
+  console.log(copyArr);
+  
