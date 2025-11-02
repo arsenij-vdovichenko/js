6aa35c96c2c9989077b9b8671f9b2049c005ada8
arsenij-vdovichenko <vdovicenkoarsenij946@gmail.com>
2025-11-02 11:32:45 +0000

    123

---
+++ js/dom.js
@@ -0,0 +1,18 @@
+const listEl = document.querySelector(".list")
+console.log(listEl.children);
+
+
+
+const listRef = document.querySelectorAll(".list .link")
+console.log(listRef);
+const list = document.querySelectorAll(".link")
+const img = document.querySelector(".img")
+
+img.src= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-AIgQp-WdQqjQCrte2O9b7jH3zbCq7KVy6w&s"
+img.alt = "rails"
+console.log(img);
+
+
+const titleRef = document.querySelector(".title")
+titleRef.textContent = "Hello World"
+console.log(titleRef);
