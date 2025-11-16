0fe16d52360540e0feab5860c1beafb8c5718fe2
arsenij-vdovichenko <vdovicenkoarsenij946@gmail.com>
2025-11-16 11:59:04 +0000

    123

---
+++ js/createdom.js
@@ -0,0 +1,34 @@
+// const listRef = document.querySelector(".js-list")
+
+// const friends = ["Andrii", "Ostap", "Olexandra", "matvii"]
+
+
+// const createItems = friends.map((item)=>{
+//     const itemRef = document.createElement("li")
+//     itemRef.textContent = item
+//     return itemRef
+// })
+
+// listRef.append(...createItems)
+
+
+
+// const images = [
+//   {
+//     url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+//     alt: "White and Black Long Fur Cat",
+//   },
+//   {
+//     url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+//     alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+//   },
+//   {
+//     url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+//     alt: "Group of Horses Running",
+//   },
+// ];
+
+// const listRef = document.getElementById("gallery")
+// const itemWorkUp = images.map(({ url, alt }) => `<li><img src="${url}" alt="${alt}"></li>`).join("");
+// listRef.insertAdjacentHTML("beforeend", itemWorkUp)
+// console.log(itemWorkUp);
